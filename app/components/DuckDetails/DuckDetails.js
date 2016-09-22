3e5a3d27f4a4b4b88211295fd189d659e2dbfa30
Justin Davidson <jutstindavidson23@gmail.com>
2016-09-22 12:04:42 +1000

    Reply ability working and saving to firebase

--- app/components/DuckDetails/DuckDetails.js
+++ app/components/DuckDetails/DuckDetails.js
@@ -1,29 +1,58 @@
 import React, { PropTypes } from 'react'
 import { DuckContainer } from 'containers'
 import { loading } from './styles.scss'
+// import { RepliesContainer } from 'containers'
+import { formatReply } from 'helpers/utils'
 
-DuckDetails.propTypes = {
-  duckId: PropTypes.string.isRequired,
-  authedUser: PropTypes.object.isRequired,
-  isFetching: PropTypes.bool.isRequired,
-  error: PropTypes.string.isRequired,
+Reply.propTypes = {
+  submit: PropTypes.func.isRequired,
 }
 
-export default function DuckDetails (props) {
+function Reply ({submit}) {
+  const handleSubmit = (e) => {
+    if (Reply.ref.value.length === 0) return
+    submit(Reply.ref.value, e)
+    Reply.ref.value = ''
+  }
+
   return (
-    <div>
-      {props.isFetching === true
-      ? <div className={loading}>{'Loading&#8230;'}</div>
-    : <div>
-        <div className="duckContent">
-          <DuckContainer duckId={props.duckId} hideLikeCount={false} hideReplyBtn={true} />
-          MAKE REPLY HERE
-        </div>
-        <div className="repliesContainer">
-          REPLIES HERE
-        </div>
-      </div>}
-      {props.error ? <p>{props.error}</p> : null}
+    <div >
+      <textarea
+        ref={(ref) => (Reply.ref = ref)}
+        maxLength={140}
+        type='text'
+        placeholder='Your reponse'/>
+      <button
+        onClick={handleSubmit} >
+          {'Submit'}
+      </button>
+    </div>
+  )
+}
+
+DuckDetails.propTypes = {
+  authedUser: PropTypes.object.isRequired,
+  duckId: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  addAndHandleReply: PropTypes.func.isRequired,
+}
+
+export default function DuckDetails ({duckId, isFetching, authedUser, error, addAndHandleReply}) {
+  return (
+    <div >
+      {isFetching === true
+        ? <div className={loading}>{'Loading&#8230;'}</div>
+        : <div >
+            <div >
+              <DuckContainer duckId={duckId} hideLikeCount={false} hideReplyBtn={true} />
+              <Reply submit={(replyText) => addAndHandleReply(duckId, formatReply(authedUser, replyText))} />
+            </div>
+            <div >
+              REPLY SECTION
+            </div>
+          </div>}
+      {error ? <p >{error}</p> : null}
     </div>
   )
 }
--- app/containers/DuckDetails/DuckDetailsContainer.js
+++ app/containers/DuckDetails/DuckDetailsContainer.js
@@ -1,22 +1,24 @@
 import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { DuckDetails } from 'components'
 import * as duckActionCreators from 'redux/modules/ducks'
 import * as likeCountActionCreators from 'redux/modules/likeCount'
+import * as repliesActionCreators from 'redux/modules/replies'
+const { func, object, string, bool } = PropTypes
 
-const DuckDetailsContainer = React.createClass ({
+const DuckDetailsContainer = React.createClass({
   propTypes: {
-    duckId: PropTypes.string.isRequired,
-    authedUser: PropTypes.object.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    error: PropTypes.string.isRequired,
-    duckAlreadyFetched: PropTypes.bool.isRequired,
-    removeFetching: PropTypes.func.isRequired,
-    fetchAndHandleDuck: PropTypes.func.isRequired,
-    initLikeFetch: PropTypes.func.isRequired,
+    authedUser: object.isRequired,
+    duckId: string.isRequired,
+    error: string.isRequired,
+    isFetching: bool.isRequired,
+    removeFetching: func.isRequired,
+    fetchAndHandleDuck: func.isRequired,
+    duckAlreadyFetched: bool.isRequired,
+    initLikeFetch: func.isRequired,
+    addAndHandleReply: func.isRequired,
   },
-
   componentDidMount () {
     this.props.initLikeFetch(this.props.duckId)
     if (this.props.duckAlreadyFetched === false) {
@@ -24,33 +26,39 @@ const DuckDetailsContainer = React.createClass ({
     } else {
       this.props.removeFetching()
     }
-  },
 
+  },
   render () {
     return (
       <DuckDetails
-        duckId={this.props.duckId}
         authedUser={this.props.authedUser}
+        duckId={this.props.duckId}
+        error={this.props.error}
         isFetching={this.props.isFetching}
-        error={this.props.error} />
+        addAndHandleReply={this.props.addAndHandleReply} />
     )
-  }
+  },
 })
 
 function mapStateToProps ({ducks, likeCount, users}, props) {
   return {
     isFetching: ducks.isFetching || likeCount.isFetching,
-    error: ducks.error || likeCount.error,
+    error: ducks.error,
     authedUser: users[users.authedId].info,
     duckId: props.routeParams.duckId,
-    duckAlreadyFetched: !!ducks[props.routeParams.duckId],
+    duckAlreadyFetched: !!ducks[props.routeParams.duckId]
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
     ...duckActionCreators,
-    ...likeCountActionCreators}, dispatch)
+    ...likeCountActionCreators,
+    ...repliesActionCreators
+  }, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(DuckDetailsContainer)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DuckDetailsContainer)
